38500cc3d571394d19082ec4369a3b6776715c02
Joshua Arvin Lat <akosijoshualat@gmail.com>
2026-05-17 05:07:00 +0800

    Add S3 JSON reader functionality

---
+++ chapter09/s3-json-reader.py
@@ -0,0 +1,24 @@
+import boto3
+import json
+from urllib.parse import urlparse
+
+s3_client = boto3.client("s3")
+
+
+def load_json_from_s3(s3_path):
+    parsed = urlparse(s3_path)
+    bucket = parsed.netloc
+    key = parsed.path.lstrip("/")
+
+    response = s3_client.get_object(Bucket=bucket, Key=key)
+    content = response["Body"].read().decode("utf-8")
+
+    return json.loads(content)
+
+
+def lambda_handler(event, context):
+    results_s3_path = event["results_s3_path"]
+
+    results = load_json_from_s3(results_s3_path)
+
+    return results
